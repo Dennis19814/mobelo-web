9f02cc6b3888c572268d7cb1fbf695cecb68de2c
ahsan-mobelo <ahsan@mobelo.dev>
2026-02-10 10:34:01 +0530

    image numbering added

--- src/lib/template-metadata.ts
+++ src/lib/template-metadata.ts
@@ -8,6 +8,7 @@
  */
 
 export type TemplateMetadata = {
+  imageNumber: string
   vertical: string
   themeId: number
   iconLibraryId: number
@@ -16,88 +17,88 @@ export type TemplateMetadata = {
 
 export const TEMPLATE_METADATA: Record<string, TemplateMetadata[]> = {
   'Verticals - Books & Media': [
-    { vertical: 'fashion5', themeId: 14, iconLibraryId: 5, borderRadius: 'custom' },
-    { vertical: 'fashion5', themeId: 18, iconLibraryId: 1, borderRadius: 'medium' },
-    { vertical: 'fashion1', themeId: 25, iconLibraryId: 6, borderRadius: 'topOnly' },
-    { vertical: 'fashion3', themeId: 7, iconLibraryId: 8, borderRadius: 'asymmetric' },
-    { vertical: 'fashion2', themeId: 4, iconLibraryId: 4, borderRadius: 'bottomOnly' },
-    { vertical: 'fashion4', themeId: 12, iconLibraryId: 3, borderRadius: 'bottomSheet' },
-    { vertical: 'fashion6', themeId: 24, iconLibraryId: 3, borderRadius: 'fullyRounded' },
-    { vertical: 'fashion7', themeId: 5, iconLibraryId: 1, borderRadius: 'custom' },
-    { vertical: 'fashion8', themeId: 9, iconLibraryId: 6, borderRadius: 'sharp' },
-    { vertical: 'fashion7', themeId: 6, iconLibraryId: 8, borderRadius: 'small' }
+    { imageNumber: '01.png', vertical: 'fashion5', themeId: 14, iconLibraryId: 5, borderRadius: 'custom' },
+    { imageNumber: '02.png', vertical: 'fashion5', themeId: 18, iconLibraryId: 1, borderRadius: 'medium' },
+    { imageNumber: '03.png', vertical: 'fashion1', themeId: 25, iconLibraryId: 6, borderRadius: 'topOnly' },
+    { imageNumber: '04.png', vertical: 'fashion3', themeId: 7, iconLibraryId: 8, borderRadius: 'asymmetric' },
+    { imageNumber: '05.png', vertical: 'fashion2', themeId: 4, iconLibraryId: 4, borderRadius: 'bottomOnly' },
+    { imageNumber: '06.png', vertical: 'fashion4', themeId: 12, iconLibraryId: 3, borderRadius: 'bottomSheet' },
+    { imageNumber: '07.png', vertical: 'fashion6', themeId: 24, iconLibraryId: 3, borderRadius: 'fullyRounded' },
+    { imageNumber: '08.png', vertical: 'fashion7', themeId: 5, iconLibraryId: 1, borderRadius: 'custom' },
+    { imageNumber: '09.png', vertical: 'fashion8', themeId: 9, iconLibraryId: 6, borderRadius: 'sharp' },
+    { imageNumber: '10.png', vertical: 'fashion7', themeId: 6, iconLibraryId: 8, borderRadius: 'small' }
   ],
   'Verticals - Electronics': [
-    { vertical: 'Fashion1', themeId: 5, iconLibraryId: 6, borderRadius: 'small' },
-    { vertical: 'fashion8', themeId: 1, iconLibraryId: 6, borderRadius: 'topOnly' },
-    { vertical: 'fashion3', themeId: 6, iconLibraryId: 8, borderRadius: 'pill' },
-    { vertical: 'fashion1', themeId: 4, iconLibraryId: 8, borderRadius: 'circular' },
-    { vertical: 'fashion5', themeId: 20, iconLibraryId: 8, borderRadius: 'topOnly' },
-    { vertical: 'Vertical 6', themeId: 18, iconLibraryId: 2, borderRadius: 'asymmetric' },
-    { vertical: 'fashion7', themeId: 25, iconLibraryId: 7, borderRadius: 'bottonOnly' },
-    { vertical: 'fashion4', themeId: 17, iconLibraryId: 8, borderRadius: 'sharp' },
-    { vertical: 'fashion5', themeId: 10, iconLibraryId: 6, borderRadius: 'medium' },
-    { vertical: 'fashion7', themeId: 3, iconLibraryId: 1, borderRadius: 'extraLarge' }
+    { imageNumber: '01.png', vertical: 'Fashion1', themeId: 5, iconLibraryId: 6, borderRadius: 'small' },
+    { imageNumber: '02.png', vertical: 'fashion8', themeId: 1, iconLibraryId: 6, borderRadius: 'topOnly' },
+    { imageNumber: '03.png', vertical: 'fashion3', themeId: 6, iconLibraryId: 8, borderRadius: 'pill' },
+    { imageNumber: '04.png', vertical: 'fashion1', themeId: 4, iconLibraryId: 8, borderRadius: 'circular' },
+    { imageNumber: '05.png', vertical: 'fashion5', themeId: 20, iconLibraryId: 8, borderRadius: 'topOnly' },
+    { imageNumber: '06.png', vertical: 'Vertical 6', themeId: 18, iconLibraryId: 2, borderRadius: 'asymmetric' },
+    { imageNumber: '07.png', vertical: 'fashion7', themeId: 25, iconLibraryId: 7, borderRadius: 'bottonOnly' },
+    { imageNumber: '08.png', vertical: 'fashion4', themeId: 17, iconLibraryId: 8, borderRadius: 'sharp' },
+    { imageNumber: '09.png', vertical: 'fashion5', themeId: 10, iconLibraryId: 6, borderRadius: 'medium' },
+    { imageNumber: '10.png', vertical: 'fashion7', themeId: 3, iconLibraryId: 1, borderRadius: 'extraLarge' }
   ],
   'Verticals - Fashion & Apparel': [
-    { vertical: 'Fashion1', themeId: 4, iconLibraryId: 1, borderRadius: 'sharp' },
-    { vertical: 'fashion2', themeId: 1, iconLibraryId: 2, borderRadius: 'medium' },
-    { vertical: 'fashion5', themeId: 2, iconLibraryId: 3, borderRadius: 'large' },
-    { vertical: 'fashion6', themeId: 9, iconLibraryId: 4, borderRadius: 'topOnly' },
-    { vertical: 'fashion7', themeId: 4, iconLibraryId: 5, borderRadius: 'bottomOnly' },
-    { vertical: 'fashion8', themeId: 15, iconLibraryId: 6, borderRadius: 'asymmetric' },
-    { vertical: 'fashion3', themeId: 11, iconLibraryId: 7, borderRadius: 'fullyRounded' },
-    { vertical: 'fashion4', themeId: 8, iconLibraryId: 8, borderRadius: 'custom' },
-    { vertical: 'fashion1', themeId: 25, iconLibraryId: 1, borderRadius: 'bottomSheet' },
-    { vertical: 'fashion5', themeId: 3, iconLibraryId: 6, borderRadius: 'large' }
+    { imageNumber: '01.png', vertical: 'Fashion1', themeId: 4, iconLibraryId: 1, borderRadius: 'sharp' },
+    { imageNumber: '02.png', vertical: 'fashion2', themeId: 1, iconLibraryId: 2, borderRadius: 'medium' },
+    { imageNumber: '03.png', vertical: 'fashion5', themeId: 2, iconLibraryId: 3, borderRadius: 'large' },
+    { imageNumber: '04.png', vertical: 'fashion6', themeId: 9, iconLibraryId: 4, borderRadius: 'topOnly' },
+    { imageNumber: '05.png', vertical: 'fashion7', themeId: 4, iconLibraryId: 5, borderRadius: 'bottomOnly' },
+    { imageNumber: '06.png', vertical: 'fashion8', themeId: 15, iconLibraryId: 6, borderRadius: 'asymmetric' },
+    { imageNumber: '07.png', vertical: 'fashion3', themeId: 11, iconLibraryId: 7, borderRadius: 'fullyRounded' },
+    { imageNumber: '08.png', vertical: 'fashion4', themeId: 8, iconLibraryId: 8, borderRadius: 'custom' },
+    { imageNumber: '09.png', vertical: 'fashion1', themeId: 25, iconLibraryId: 1, borderRadius: 'bottomSheet' },
+    { imageNumber: '10.png', vertical: 'fashion5', themeId: 3, iconLibraryId: 6, borderRadius: 'large' }
   ],
   'Verticals - Food & Grocery': [
-    { vertical: 'Fashion3', themeId: 3, iconLibraryId: 4, borderRadius: 'small' },
-    { vertical: 'fashion4', themeId: 7, iconLibraryId: 4, borderRadius: 'pill' },
-    { vertical: 'fashion5', themeId: 4, iconLibraryId: 5, borderRadius: 'bottomSheet' },
-    { vertical: 'fashion1', themeId: 24, iconLibraryId: 2, borderRadius: 'fullyRounded' },
-    { vertical: 'fashion6', themeId: 25, iconLibraryId: 3, borderRadius: 'circular' },
-    { vertical: 'fashion1', themeId: 5, iconLibraryId: 6, borderRadius: 'custom' },
-    { vertical: 'fashion1', themeId: 9, iconLibraryId: 4, borderRadius: 'sharp' },
-    { vertical: 'fashion8', themeId: 6, iconLibraryId: 2, borderRadius: 'small' },
-    { vertical: 'fashion4', themeId: 14, iconLibraryId: 8, borderRadius: 'medium' },
-    { vertical: 'fashion7', themeId: 15, iconLibraryId: 1, borderRadius: 'large' }
+    { imageNumber: '01.png', vertical: 'Fashion3', themeId: 3, iconLibraryId: 4, borderRadius: 'small' },
+    { imageNumber: '02.png', vertical: 'fashion4', themeId: 7, iconLibraryId: 4, borderRadius: 'pill' },
+    { imageNumber: '03.png', vertical: 'fashion5', themeId: 4, iconLibraryId: 5, borderRadius: 'bottomSheet' },
+    { imageNumber: '04.png', vertical: 'fashion1', themeId: 24, iconLibraryId: 2, borderRadius: 'fullyRounded' },
+    { imageNumber: '05.png', vertical: 'fashion6', themeId: 25, iconLibraryId: 3, borderRadius: 'circular' },
+    { imageNumber: '06.png', vertical: 'fashion1', themeId: 5, iconLibraryId: 6, borderRadius: 'custom' },
+    { imageNumber: '07.png', vertical: 'fashion1', themeId: 9, iconLibraryId: 4, borderRadius: 'sharp' },
+    { imageNumber: '08.png', vertical: 'fashion8', themeId: 6, iconLibraryId: 2, borderRadius: 'small' },
+    { imageNumber: '09.png', vertical: 'fashion4', themeId: 14, iconLibraryId: 8, borderRadius: 'medium' },
+    { imageNumber: '10.png', vertical: 'fashion7', themeId: 15, iconLibraryId: 1, borderRadius: 'large' }
   ],
   'Verticals - General Retail': [
-    { vertical: 'Fashion1', themeId: 5, iconLibraryId: 6, borderRadius: 'small' },
-    { vertical: 'fashion8', themeId: 1, iconLibraryId: 6, borderRadius: 'topOnly' },
-    { vertical: 'fashion3', themeId: 6, iconLibraryId: 8, borderRadius: 'pill' },
-    { vertical: 'fashion1', themeId: 4, iconLibraryId: 8, borderRadius: 'circular' },
-    { vertical: 'fashion5', themeId: 20, iconLibraryId: 8, borderRadius: 'topOnly' },
-    { vertical: 'Vertical 6', themeId: 18, iconLibraryId: 2, borderRadius: 'asymmetric' },
-    { vertical: 'fashion7', themeId: 25, iconLibraryId: 7, borderRadius: 'bottonOnly' },
-    { vertical: 'fashion4', themeId: 17, iconLibraryId: 8, borderRadius: 'sharp' },
-    { vertical: 'fashion5', themeId: 10, iconLibraryId: 6, borderRadius: 'medium' },
-    { vertical: 'fashion7', themeId: 3, iconLibraryId: 1, borderRadius: 'extraLarge' }
+    { imageNumber: '01.png', vertical: 'Fashion1', themeId: 5, iconLibraryId: 6, borderRadius: 'small' },
+    { imageNumber: '02.png', vertical: 'fashion8', themeId: 1, iconLibraryId: 6, borderRadius: 'topOnly' },
+    { imageNumber: '03.png', vertical: 'fashion3', themeId: 6, iconLibraryId: 8, borderRadius: 'pill' },
+    { imageNumber: '04.png', vertical: 'fashion1', themeId: 4, iconLibraryId: 8, borderRadius: 'circular' },
+    { imageNumber: '05.png', vertical: 'fashion5', themeId: 20, iconLibraryId: 8, borderRadius: 'topOnly' },
+    { imageNumber: '06.png', vertical: 'Vertical 6', themeId: 18, iconLibraryId: 2, borderRadius: 'asymmetric' },
+    { imageNumber: '07.png', vertical: 'fashion7', themeId: 25, iconLibraryId: 7, borderRadius: 'bottonOnly' },
+    { imageNumber: '08.png', vertical: 'fashion4', themeId: 17, iconLibraryId: 8, borderRadius: 'sharp' },
+    { imageNumber: '09.png', vertical: 'fashion5', themeId: 10, iconLibraryId: 6, borderRadius: 'medium' },
+    { imageNumber: '10.png', vertical: 'fashion7', themeId: 3, iconLibraryId: 1, borderRadius: 'extraLarge' }
   ],
   'Verticals - Health & Beauty': [
-    { vertical: 'fashion4', themeId: 9, iconLibraryId: 8, borderRadius: 'topOnly' },
-    { vertical: 'fashion2', themeId: 4, iconLibraryId: 8, borderRadius: 'large' },
-    { vertical: 'fashion3', themeId: 7, iconLibraryId: 8, borderRadius: 'extraLarge' },
-    { vertical: 'fashion4', themeId: 9, iconLibraryId: 8, borderRadius: 'topOnly' },
-    { vertical: 'fashion4', themeId: 25, iconLibraryId: 6, borderRadius: 'pill' },
-    { vertical: 'fashion5', themeId: 24, iconLibraryId: 1, borderRadius: 'circular' },
-    { vertical: 'fashion6', themeId: 15, iconLibraryId: 4, borderRadius: 'asymmetric' },
-    { vertical: 'fashion7', themeId: 12, iconLibraryId: 4, borderRadius: 'custom' },
-    { vertical: 'fashion8', themeId: 6, iconLibraryId: 5, borderRadius: 'large' },
-    { vertical: 'fashion7', themeId: 3, iconLibraryId: 2, borderRadius: 'sharp' }
+    { imageNumber: '01.png', vertical: 'fashion4', themeId: 9, iconLibraryId: 8, borderRadius: 'topOnly' },
+    { imageNumber: '02.png', vertical: 'fashion2', themeId: 4, iconLibraryId: 8, borderRadius: 'large' },
+    { imageNumber: '03.png', vertical: 'fashion3', themeId: 7, iconLibraryId: 8, borderRadius: 'extraLarge' },
+    { imageNumber: '04.png', vertical: 'fashion4', themeId: 9, iconLibraryId: 8, borderRadius: 'topOnly' },
+    { imageNumber: '05.png', vertical: 'fashion4', themeId: 25, iconLibraryId: 6, borderRadius: 'pill' },
+    { imageNumber: '06.png', vertical: 'fashion5', themeId: 24, iconLibraryId: 1, borderRadius: 'circular' },
+    { imageNumber: '07.png', vertical: 'fashion6', themeId: 15, iconLibraryId: 4, borderRadius: 'asymmetric' },
+    { imageNumber: '08.png', vertical: 'fashion7', themeId: 12, iconLibraryId: 4, borderRadius: 'custom' },
+    { imageNumber: '09.png', vertical: 'fashion8', themeId: 6, iconLibraryId: 5, borderRadius: 'large' },
+    { imageNumber: '10.png', vertical: 'fashion7', themeId: 3, iconLibraryId: 2, borderRadius: 'sharp' }
   ],
   'Verticals - Sports & Fitness': [
-    { vertical: 'Fashion2', themeId: 15, iconLibraryId: 3, borderRadius: 'extraLarge' },
-    { vertical: 'fashion1', themeId: 9, iconLibraryId: 1, borderRadius: 'pill' },
-    { vertical: 'fashion5', themeId: 5, iconLibraryId: 5, borderRadius: 'bottomSheet' },
-    { vertical: 'fashion1', themeId: 6, iconLibraryId: 1, borderRadius: 'pill' },
-    { vertical: 'fashion3', themeId: 25, iconLibraryId: 6, borderRadius: 'circular' },
-    { vertical: 'fashion8', themeId: 12, iconLibraryId: 8, borderRadius: 'bottomSheet' },
-    { vertical: 'fashion6', themeId: 22, iconLibraryId: 1, borderRadius: 'medium' },
-    { vertical: 'fashion4', themeId: 4, iconLibraryId: 2, borderRadius: 'topOnly' },
-    { vertical: 'fashion5', themeId: 7, iconLibraryId: 2, borderRadius: 'bottomOnly' },
-    { vertical: 'fashion7', themeId: 24, iconLibraryId: 5, borderRadius: 'fullyRounded' }
+    { imageNumber: '01.png', vertical: 'Fashion2', themeId: 15, iconLibraryId: 3, borderRadius: 'extraLarge' },
+    { imageNumber: '02.png', vertical: 'fashion1', themeId: 9, iconLibraryId: 1, borderRadius: 'pill' },
+    { imageNumber: '03.png', vertical: 'fashion5', themeId: 5, iconLibraryId: 5, borderRadius: 'bottomSheet' },
+    { imageNumber: '04.png', vertical: 'fashion1', themeId: 6, iconLibraryId: 1, borderRadius: 'pill' },
+    { imageNumber: '05.png', vertical: 'fashion3', themeId: 25, iconLibraryId: 6, borderRadius: 'circular' },
+    { imageNumber: '06.png', vertical: 'fashion8', themeId: 12, iconLibraryId: 8, borderRadius: 'bottomSheet' },
+    { imageNumber: '07.png', vertical: 'fashion6', themeId: 22, iconLibraryId: 1, borderRadius: 'medium' },
+    { imageNumber: '08.png', vertical: 'fashion4', themeId: 4, iconLibraryId: 2, borderRadius: 'topOnly' },
+    { imageNumber: '09.png', vertical: 'fashion5', themeId: 7, iconLibraryId: 2, borderRadius: 'bottomOnly' },
+    { imageNumber: '10.png', vertical: 'fashion7', themeId: 24, iconLibraryId: 5, borderRadius: 'fullyRounded' }
   ]
 }
 
